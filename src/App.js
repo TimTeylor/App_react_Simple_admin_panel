45bcd39c8fc72d19d9159fc1bb3c80b0c90ecbfc
TimTeylor <timteillor@gmail.com>
2018-09-09 15:44:39 +0000

    dev 0.1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import './store';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>123456</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
